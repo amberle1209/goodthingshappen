8e7e06b0409248c5063f5bca9967255a690560c2
amberliu <amber.le1209@gmail.com>
2026-05-21 18:55:35 +0800

    Set bookkeeping intake metadata

---
+++ src/app/bookkeeping-intake/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <title>Bookkeeping Intake Cleanup</title>
+      <meta
+        name="description"
+        content="A lightweight intake cleanup service for bookkeeping firms that need cleaner client packets and faster month-end close."
+      />
+    </>
+  );
+}
